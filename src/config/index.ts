867c470b2bbfcf617163ab70f65e8dc9de9b633d
Pascual Hdez <pascual.hernandez@wizeline.com>
2023-02-10 11:41:25 -0600

    add examples for environments

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === "true";
-export const { NODE_ENV, PORT, LOG_DIR } = process.env;
+export const { NODE_ENV, PORT, LOG_DIR, DATABASE_URL } = process.env;
